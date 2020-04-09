ed668e066660a3c41d60620788d101d8a3874b9c
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-04-09 13:22:40 -0400

    Added algorithm into weight_warning() method
    I changed the weight_warning() method to utilize the algorithm written in cs390_alg.py. Works for me on my machine. I just copied & pasted the code, just had to change a few variable names but no big deal. 
    
    Let me know if you have any questions. 
    
    -Jay

--- import_patient_info.py
+++ import_patient_info.py
@@ -28,316 +28,40 @@ class Patient:
         self.meds = medictions
         self.issue = warnings
         self.num = age
+        self.ages=[0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+        self.heights=list(range(57,85))
+        self.min_weight=[6.0, 13.3, 15.8, 18.1, 19.9, 23.0, 23.4, 26.5, 31.5, 34.0, 39.5, 44.0, 49.5,
+                    57.0, 62.0, 70.5, 81.5, 91.5, 101.0, 105.0, 115.0, 118.0, 120.0]
+        self.max_weight=[15.4, 18.3, 20.1, 21.8, 24.6, 31.0, 36.0, 40.5, 45.5, 50.5, 56.5, 63.0, 70.5,
+                    78.5, 88.0, 100.0, 112.0, 123.5, 134.0, 142.0, 147.5, 152.0, 155.0]
+        self.go=0
 
     def weight_warning(self):
-        if self.num <= .25 and self.weight >= 15.4:
-            print("\nOVERWEIGHT")
-
-        if self.num <= .25 and self.weight <= 6.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= .25) and (self.num <= .5) and self.weight >= 18.3:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= .25) and (self.num <= .5) and self.weight <= 13.3:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= .5) and (self.num <= .75) and self.weight >= 20.1:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= .5) and (self.num <= .75) and self.weight <= 15.8:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= .75) and (self.num <= 1) and self.weight >= 21.8:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= .75) and (self.num <= 1) and self.weight <= 18.1:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 1) and (self.num <= 1.5) and self.weight >= 24.6:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 1) and (self.num <= 1.5) and self.weight <= 19.9:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 1.5) and (self.num <= 2) and self.weight >= 31.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 1.5) and (self.num <= 2) and self.weight <= 23.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 2) and (self.num <= 3) and self.weight <= 36.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 2) and (self.num <= 3) and self.weight <= 23.4:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 3) and (self.num <= 4) and self.weight >= 40.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 3) and (self.num <= 4) and self.weight <= 26.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 4) and (self.num <= 5) and self.weight >= 45.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 4) and (self.num <= 5) and self.weight <= 31.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 5) and (self.num <= 6) and self.weight >= 50.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 5) and (self.num <= 6) and self.weight <= 34.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 6) and (self.num <= 7) and self.weight >= 56.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 6) and (self.num <= 7) and self.weight <= 39.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 7) and (self.num <= 8) and self.weight >= 63.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 7) and (self.num <= 8) and self.weight <= 44.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 8) and (self.num <= 9) and self.weight >= 70.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 8) and (self.num <= 9) and self.weight <= 49.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 9) and (self.num <= 10) and self.weight >= 78.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 9) and (self.num <= 10) and self.weight <= 57.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 10) and (self.num <= 11) and self.weight >= 88.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 10) and (self.num <= 11) and self.weight <= 62.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 11) and (self.num <= 12) and self.weight >= 100.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 11) and (self.num <= 12) and self.weight <= 70.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 12) and (self.num <= 13) and self.weight >= 112.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 12) and (self.num <= 13) and self.weight <= 81.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 13) and (self.num <= 14) and self.weight >= 123.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 13) and (self.num <= 14) and self.weight <= 91.5:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 14) and (self.num <= 15) and self.weight >= 134.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 14) and (self.num <= 15) and self.weight <= 101.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 15) and (self.num <= 16) and self.weight >= 142.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 15) and (self.num <= 16) and self.weight <= 105.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 16) and (self.num <= 17) and self.weight >= 147.5:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 16) and (self.num <= 17) and self.weight <= 115.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 17) and (self.num <= 18) and self.weight >= 152.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 17) and (self.num <= 18) and self.weight <= 118.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 18) and (self.num <= 19) and self.weight >= 155.0:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 18) and (self.num <= 19) and self.weight <= 120.0:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 57) and self.weight >= 103:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 57) and self.weight <= 72:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 58) and self.weight >= 110:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 58) and self.weight <= 77:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 59) and self.weight >= 117:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 59) and self.weight <= 81:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 60) and self.weight >= 123:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 60) and self.weight <= 86:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 61) and self.weight >= 130:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 61) and self.weight <= 90:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 62) and self.weight >= 136:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 62) and self.weight <= 95:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 63) and self.weight >= 143:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 63) and self.weight <= 99:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 64) and self.weight >= 150:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 64) and self.weight <= 104:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 65) and self.weight >= 156:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 65) and self.weight <= 108:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 66) and self.weight >= 163:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 66) and self.weight <= 113:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 67) and self.weight >= 169:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 67) and self.weight <= 117:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 68) and self.weight >= 176:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 68) and self.weight <= 122:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 69) and self.weight >= 183:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 69) and self.weight <= 126:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 70) and self.weight >= 189:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 70) and self.weight <= 131:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 71) and self.weight >= 196:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 71) and self.weight <= 135:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 72) and self.weight >= 202:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 72) and self.weight <= 140:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 73) and self.weight >= 209:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 73) and self.weight <= 144:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 74) and self.weight >= 216:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 74) and self.weight <= 149:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 75) and self.weight >= 216:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 75) and self.weight <= 153:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 76) and self.weight >= 229:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 76) and self.weight <= 158:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 77) and self.weight >= 235:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 77) and self.weight <= 162:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 78) and self.weight >= 242:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 78) and self.weight <= 167:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 79) and self.weight >= 249:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 79) and self.weight <= 171:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 80) and self.weight >= 255:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 80) and self.weight <= 176:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 81) and self.weight >= 262:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 81) and self.weight <= 180:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 82) and self.weight >= 268:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 82) and self.weight <= 185:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 83) and self.weight >= 275:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 83) and self.weight <= 189:
-            print("\nUNDERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 84) and self.weight >= 280:
-            print("\nOVERWEIGHT")
-
-        if (self.num >= 19) and (self.height <= 84) and self.weight <= 194:
-            print("\nUNDERWEIGHT")
-
-        else:
-            print("\nHEALTHY WEIGHT")
+        # For this method to work it's very important that the user inputs an age that is an integer, not a float
+        for i in range(0,len(self.ages)):
+            if self.ages[i]==self.num:
+                self.go=i
+                break
+            elif self.num>=19:
+                self.go=19
+                break
+
+        if self.go==19:
+            self.min_weight=[72, 77, 81, 86, 90, 95, 99, 104, 108, 113, 117, 122, 126, 131,
+                             135, 140, 144, 149, 153, 158, 162, 167, 171, 176, 180, 185, 189, 194]
+            self.max_weight=[103, 110, 117, 123, 130, 136, 143, 150, 156, 163, 169, 176, 183, 189,
+                             196, 202, 209, 216, 223, 229, 235, 242, 249, 255, 262, 268, 275, 280]
+            for i in range(0,len(self.heights)):
+                if self.heights[i]==self.height:
+                    self.go=i
+                    break
+                
+        if int(self.min_weight[self.go])<=self.weight<=int(self.max_weight[self.go]):
+            print('healthy weight')
+        if self.weight < self.min_weight[self.go]:
+            print('underweight')
+        if self.weight > self.max_weight[self.go]:
+            print('overweight')
 
     def blood_warning(self):
         if (self.sys <= 90) and (self.dia <= 60):
@@ -380,8 +104,8 @@ class Patient:
             print(f"This patient is at risk of the following: {family_medical}")
 
 
-bollinger_kalei = Patient('Kalei', 'Bollinger', '06-05-2001', 125, 65, 'Female', '120', '80', 'O-',
+bollinger_kalei = Patient('Kalei', 'Bollinger', '06-05-2001', 125, 65, 'Female', 120, 80, 'O-',
                           ['Abnormal Aortic Aneurysm', 'Chiari Malformation', 'Crohns Disease'], 18,
                           ['CRPS', 'Sacroilitis'], ['Bees', 'Tramidol'], None, ['Xylitol', 'Humara'])
 
-bollinger_kalei.blood_warning()
+bollinger_kalei.weight_warning()
